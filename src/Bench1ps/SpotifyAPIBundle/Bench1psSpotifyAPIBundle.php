205ee57ebc3cb1d5eb5474791aba31a00cdfb014
Benjamin Fraud <benjamin.fraud@blablacar.com>
2018-11-08 10:47:32 +0100

    Bootstrap

---
+++ src/Bench1ps/SpotifyAPIBundle/Bench1psSpotifyAPIBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Blablacar\Bundle\InsuranceBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class Bench1psSpotifyAPIBundle extends Bundle
+{
+}
---
+++ src/Bench1ps/SpotifyAPIBundle/DependencyInjection/Bench1psSpotifyAPIExtension.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Blablacar\Bundle\InsuranceBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class Bench1psSpotifyAPIExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+    }
+}
